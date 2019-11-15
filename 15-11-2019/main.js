cff8fa58232c93ce62b1e6d3ef4d12503de3129c
Md Jonaed Hasan <jonaed.com.bd@gmail.com>
2019-11-15 22:37:54 +0600

    15/11/2019 class added

---
+++ 15-11-2019/main.js
@@ -0,0 +1,52 @@
+// or condition
+
+/* if (1<0 || 5<4 || 2>10) {
+  console.log("You are passed");
+} else {
+  console.log("You are failed!");
+} */
+
+// and condition
+
+/* if (1>0 && 5>4 && 2<10) {
+  console.log("You are passed");
+} else {
+  console.log("You are failed!");
+} */
+
+// not condition
+
+/* if ((!(5<4) && 1>0 || 2>10) && 2>10) {
+  console.log("You are passed");
+} else {
+  console.log("You are failed!");
+}
+ */
+
+// last class problem
+
+/* const name = prompt("What is your name?");
+if (name === "admin") {
+  const pass = prompt(`Hi ${name} please enter your password`);
+  if (pass === '1234') {
+    alert("Welcome admin");
+  } else {
+    alert("I think you are not admin. Better try next time.")
+  }
+} else {
+  alert("You are not admin. I can't give you access");
+} */
+
+// loops
+
+// do not try this
+
+/* 
+while(true) {
+  console.log("something!");
+} */
+
+/* for (let i = 0; i < 15; i++) {
+  console.log(`You are at ${i}'th position`);
+} */
+
