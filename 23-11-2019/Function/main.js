8cc2f50a0e00645e24860de490c5ccb095a632bd
Md Jonaed Hasan <jonaed.com.bd@gmail.com>
2019-11-23 22:58:02 +0600

    FEAT: 23-11-2019 class added

---
+++ 23-11-2019/Function/main.js
@@ -0,0 +1,14 @@
+// es5 function
+function addition(a, b) {
+  return a + b;
+}
+
+// console.log(addition(5, 18));
+
+// es6 function
+
+multiply = (a, b) => {
+  return a * b;
+};
+
+// console.log(multiply(25, 15));
---
+++ 23-11-2019/switch-case/main.js
@@ -0,0 +1,15 @@
+const name = 'Taijul';
+
+switch (name) {
+  case 'Jonaed':
+    alert ('You are Jonaed');
+    break;
+  case 'Nazmul':
+    alert ('You are Nazmul');
+    break;
+  case 'Taijul':
+    alert ('You are Taijul');
+    break;
+  default:
+    alert ('You are Shawon');      
+}
